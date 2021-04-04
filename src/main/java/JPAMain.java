9d1db4b068be21f2d7a79652d98ede748483a768
hgs-study <hgstudy_@naver.com>
2021-04-04 21:21:45 +0900

    [TypeQuery,Query] 반환 타입이 명확한지 구분

--- src/main/java/JPAMain.java
+++ src/main/java/JPAMain.java
@@ -1,9 +1,6 @@
 import jpql.Member;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 public class JPAMain {
 
@@ -17,7 +14,12 @@ public class JPAMain {
         try{
             Member member = new Member();
             member.setUsername("현건수");
+            member.setAge(10);
             em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class); // 반환타입 Member
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class); // 변환타입 명확 username은 String
+            Query query3 = em.createQuery("select m.username, m.age from Member m"); //반환 타입이 명확하지 않음 username ->String , age -> int 이기 때문에
 
             tx.commit();
         }catch (Exception e){
