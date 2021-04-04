97e543c97710b4dc6ac62257ef19b37048b5f892
hgs-study <hgstudy_@naver.com>
2021-04-04 21:34:37 +0900

    [TypeQuery,Query] 반환 값 getResultList, getSingleResult -> 메서드 체이닝으로 변경

--- src/main/java/JPAMain.java
+++ src/main/java/JPAMain.java
@@ -18,14 +18,15 @@ public class JPAMain {
             member.setAge(10);
             em.persist(member);
 
-            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class); // 반환타입 Member
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class); // 변환타입 명확 username은 String
-            Query query3 = em.createQuery("select m.username, m.age from Member m"); //반환 타입이 명확하지 않음 username ->String , age -> int 이기 때문에
+            //메서드 체이닝
+            Member singleResult = em.createQuery("select m from Member m where m.username=:username", Member.class) // 반환타입 Member
+                    .setParameter("username", "현건수")
+                    .getSingleResult();
+            System.out.println(singleResult.getUsername());
 
-            List<Member> resultList = query.getResultList(); // 결과가 없으면 빈 리스트 반환 NPE 안뜸
-            Member singleResult = query.getSingleResult(); //결과가 정확히 하나, 단일 객체 반환 ,
 
-            resultList.forEach(System.out::println);
+
+
             System.out.println(singleResult);
 
             tx.commit();
