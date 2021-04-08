a2ec9542702cc54027e808c808559d54d072dee6
hgs-study <hgstudy_@naver.com>
2021-04-08 19:48:07 +0900

    [엔티티 직접 사용] 엔티티를 직접 사용하면 엔티티의 기본키값을 사용한다 (ex) m-> m.id 사용

--- src/main/java/JPAMain.java
+++ src/main/java/JPAMain.java
@@ -39,28 +39,17 @@ public class JPAMain {
             em.flush();
             em.clear();
 
+            //엔티티를 직접 사용하면 엔티티의 기본키값을 사용한다 (ex) m-> m.id 사용
+            String query = "select m from Member m where m = :member";
 
-            String query = "select DISTINCT t from Team t";
+            Member member1 = em.createQuery(query, Member.class)
+                    .setParameter("member", member)
+                    .getSingleResult();
 
-            //DB엔 여러 ROW가 있는데 PAGE 1개만 가져와 하면 JPA는 그대로 1개만 가져온다.
-            //그 1개에 객체 그래프를 다 가져오는 속성때문에 여러 객체(ROW)를 다 가져온다.
-            List<Team> queryList = em.createQuery(query, Team.class)
-                    .setFirstResult(0)
-                    .setMaxResults(2)
-                    .getResultList();
+            System.out.println("member1 = " + member1);
 
-            System.out.println("queryList.size() = " + queryList.size());
 
-            //컬렉션 페치조인 일대다에선 데이터 증폭돼서 나옴
-            //이유 : teamA에 2명의 회원이 있으면 똑같은 데이터가 2개나옴 (회원이 2명이기 때문에 2줄이 나오는 것)
-            for (Team team : queryList) {
-                System.out.println("team.getName() = " + team.getName() +", members = "+team.getMembers().size());
-                for (Member teamMember : team.getMembers()) {
-                    System.out.println("-> teamMember = " + teamMember);
-                }
-            }
-
-           tx.commit();
+            tx.commit();
         }catch (Exception e){
             tx.rollback();
             e.printStackTrace();
